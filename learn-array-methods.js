88229e0c8228e158cc02180d04a0bc1e1900237f
ajshiff <ajshiffler@gmail.com>
2018-04-27 10:23:32 -0600

    Concat, forEach, filter, and reduce examples

--- learn-array-methods.js
+++ learn-array-methods.js
@@ -1,5 +1,6 @@
-//comment
-//var pointValue = [5, 5, 5, 10, 3, 3, 3, 3, 3];
+/**********************************************************************
+ * 
+ **********************************************************************/
 var array1 = ['a','b','c'];
 var array2 = ['d','e','f'];
 var array3 = array1.concat(array2);
@@ -12,3 +13,21 @@ array3.forEach(function(element, index){console.log(element + index)});
 //console.log(array3.includes('a'[0]));
 var element = 2;
 console.log(array4.every(function(element){return element===2;}));
+
+/**********************************************************************
+ * 
+ **********************************************************************/
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+function isLongEnough(length) {
+    return length > 5;
+}
+  var result = words.filter(taco => isLongEnough(taco.length));
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
+
+/**********************************************************************
+ * 
+ **********************************************************************/
+console.log(array1.reduce((total, current) => total+current));
