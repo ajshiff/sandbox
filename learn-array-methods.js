2775e9a7a8f2163b4c271f3210f807c0a785c3ab
ajshiff <ajshiffler@gmail.com>
2018-04-27 09:03:51 -0600

    Array Methods Update 1

--- learn-array-methods.js
+++ learn-array-methods.js
@@ -1,5 +1,14 @@
 //comment
-var test = false;
-if (test){null;}
-console.log('Hello World!');
-console.log('MasterGramps');
+//var pointValue = [5, 5, 5, 10, 3, 3, 3, 3, 3];
+var array1 = ['a','b','c'];
+var array2 = ['d','e','f'];
+var array3 = array1.concat(array2);
+var array4 = [2,2,4,2,2];
+console.log(array1.concat(array2));
+console.log(array1);
+console.log(array2);
+console.log(array3);
+array3.forEach(function(element, index){console.log(element + index)});
+//console.log(array3.includes('a'[0]));
+var element = 2;
+console.log(array4.every(function(element){return element===2;}));
