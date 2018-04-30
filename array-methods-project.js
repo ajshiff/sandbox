6e86fe5b92c81f0bf3ce1497b61c80c09cd86001
ajshiff <ajshiffler@gmail.com>
2018-04-30 10:15:56 -0600

    Add Date Feature to transactionHistory and dependent functions

--- array-methods-project.js
+++ array-methods-project.js
@@ -1,25 +1,35 @@
-console.log("Practical array.push(): Transaction History");
+//console.log("Practical array.push(): Transaction History");
 var transactionHistory = [
-    ["Walmart", 50],
-    ["Taco Bell", 10],
-    ["Fat Cats" , 25]
+    [new Date(2018, 2, 28), "Walmart", 50],
+    [new Date(2018, 3, 2), "Taco Bell", 10],
+    [new Date(2018, 3, 30), "Fat Cats" , 25]
 ];
+
 /***************************************************************
- * displayTransaction is a quick way to display elements from 
- * your transaction history
+ * Print months takes a number input between 0-11 and returns the month name in string.
  ***************************************************************/
-function displayTransaction(store, price){
-    console.log("Store: " + store + " --- Price: " + price);
+function printMonth(monthNumber){
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[monthNumber]
 }
 
 /***************************************************************
- * addAndDisplayNewTransaction will push a new element to the
+ * displayTransaction() is a quick way to display elements from 
+ * your transaction history
+ ***************************************************************/
+function displayTransaction(date, store, price){
+
+    console.log("Date: " + date.getFullYear() + ' ' + printMonth(date.getMonth()) + ' ' + date.getDate() + " --- Store: " + store + " --- Price: " + price);
+}
+
+/***************************************************************
+ * addAndDisplayNewTransaction() will push a new element to the
  * transactionHistory array, then will display that new element
  * from the array.
  ***************************************************************/
-function addAndDisplayNewTransaction(store, price){
-    let newLength = transactionHistory.push([store, price])-1;
-    displayTransaction(transactionHistory[newLength][0], transactionHistory[newLength][1]);
+function addAndDisplayNewTransaction(date, store, price){
+    let newLength = transactionHistory.push([date, store, price])-1;
+    displayTransaction(transactionHistory[newLength][0], transactionHistory[newLength][1], transactionHistory[newLength][2]);
 }
 
 /***************************************************************
@@ -28,7 +38,8 @@ function addAndDisplayNewTransaction(store, price){
  * and displays that specific transaction.
  ***************************************************************/
 function displaySpecificTransaction(transactionNumber){
-
+    let value = transactionHistory[transactionNumber];
+    displayTransaction(value[0], value[1], value[2]);
 }
 
 /***************************************************************
@@ -37,11 +48,11 @@ function displaySpecificTransaction(transactionNumber){
  ***************************************************************/
 function displayAllTransactions(){
     transactionHistory.forEach(element => {
-        displayTransaction(element[0], element[1]);
+        displayTransaction(element[0], element[1], element[2]);
     });
 }
 
 displayAllTransactions();
-addAndDisplayNewTransaction("Rent", 300);
-addAndDisplayNewTransaction("Car Insurance", 80);
-
+addAndDisplayNewTransaction(new Date(2018, 4, 13), "Rent", 300);
+addAndDisplayNewTransaction(new Date(2017, 10, 31), "Car Insurance", 80);
+displaySpecificTransaction(0);
