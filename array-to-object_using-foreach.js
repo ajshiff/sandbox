0550d0d8312b5479a15db91df18ab2ea472bb067
ajshiff <ajshiffler@gmail.com>
2018-05-07 10:35:48 -0600

    Complete Product. ~10 lines to solution.

---
+++ array-to-object_using-foreach.js
@@ -0,0 +1,40 @@
+const before = [
+    {
+        key: "year", value: "2016" //0
+    },
+    {
+        key: "make", value: "Porsche" //1
+    },
+    {
+        key: "model", value: "911 R", //2
+    },
+    {
+        key: "color", value: "white", //3
+    },
+    {
+        key: "msrp", value: "$184,900" //4
+    }
+]
+console.log("BEFORE");console.log(before);console.log();
+
+/*START EDITS*/
+function sendElementsToObject(array){
+    var object = {};
+    array.forEach(element => {
+        var key = element.key;
+        var value = element.value
+        object[key] = value;
+    });
+    return object;}
+before.pop();
+const after = sendElementsToObject(before);
+/*END EDITS*/
+
+var goal = {
+    year: "2016",
+    make: "Porsche",
+    model: "911 R",
+    color: "white"}
+/*Logs*/
+console.log("AFTER");console.log(after);console.log();
+console.log("GOAL");console.log(goal);console.log();
