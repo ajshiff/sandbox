8818b151f4211708774d38e9daca9eb0ad9cff14
ajshiff <ajshiffler@gmail.com>
2018-05-07 11:56:35 -0600

    Add hard-code example input to base dynamic input on. It works accurately.

--- citiesTraining.js
+++ citiesTraining.js
@@ -118,7 +118,27 @@ Hillion,1363900,Chizered,Westuming`;
  * This function should return an array containing the
  * converted data.
  ******************************************************/
-function convertData(data) {}
+//Info comes in this order and format: Name,Population,State,Country\'n
+//It should be in ---[{Name, States[{Name, Cities[{Name, Population}]}]}]---
+/*
+[
+    {Name: "", States:
+        [
+            {Name: "", Cities:
+                [
+                    {Name: "", Population: #######}
+                ]
+            }
+        ]
+    }
+]
+*/
+function convertData(data) {
+    //ConvertedDataTemplate Correctly Displays the values of each field. 
+    //It's been checked. Make sure the CVS Data follows the same pattern.
+    convertedDataTemplate = [{Name: "NameOfCountry", States:[{Name: "NameOfState", Cities:[{Name: "NameOfCity", Population: 987650}]}]}];
+    convertedData = [];
+}
 
 /******************************************************
  *                    sortData
@@ -184,7 +204,7 @@ function main() {
     var data = d3.csvParse(csvData);
     var convertedData = convertData(data);
     var sortedData = sortData(convertedData);
-    //display(sortedData);
+    display(sortedData);
 }
 
 main();
