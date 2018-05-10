35c73f1c5a15d627f3d776511d8f62cd3eaca5e5
ajshiff <ajshiffler@gmail.com>
2018-05-10 12:01:49 -0600

    CSV Data to array Working.

--- citiesTraining.js
+++ citiesTraining.js
@@ -139,7 +139,7 @@ function convertData(data) {
     //It's been checked. Make sure the CVS Data follows the same pattern.
     //convertedDataTemplate = [{Name: "NameOfCountry", States:[{Name: "NameOfState", Cities:[{Name: "NameOfCity", Population: 987650}]}]}];
     //Data is packaged in this order: NameCity,Population,State,Country
-    var convertedData = data.reduce((acc, curr) => {console.log(curr);}, []);
+    var convertedData = data.reduce((acc, curr) => acc.concat(curr), []);
     console.log(convertedData);
     return convertedData;
 }
